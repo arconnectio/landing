ad34cbac24b7c611e0e221d56e715eb4b1339cfe
Marton Lederer <marton@lederer.hu>
2022-10-25 20:20:28 +0200

    feat: base

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
