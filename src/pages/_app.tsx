2e556569ccf517bcb66e1165dd4421d8fd3d6204
Marton Lederer <marton@lederer.hu>
2023-03-20 10:18:48 +0100

    feat: SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import type { AppProps } from "next/app";
 import { Manrope } from "next/font/google";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={manrope.className}>
+      <Head>
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+        <meta name="theme-color" content="#AB9AFF" />
+        <meta name="msapplication-TileColor" content="#AB9AFF" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <>test</>;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="ArConnect is the first wallet for Arweave, providing secure wallet and asset management" />
+        <title>ArConnect - Arweave Wallet</title>
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="ArConnect - Arweave Wallet" />
+        <meta name="twitter:site" content="@arconnectio" />
+        <meta name="twitter:description" content="ArConnect is the first wallet for Arweave, providing secure wallet and asset management" />
+        <meta name="twitter:image" content="https://arconnect.io/og.png" />
+        <meta name="twitter:image:alt" content="ArConnect is the first wallet for Arweave, providing secure wallet and asset management" />
+
+        <meta property="og:title" content="ArConnect - Arweave Wallet" />
+        <meta property="og:description" content="ArConnect is the first wallet for Arweave, providing secure wallet and asset management" />
+        <meta property="og:url" content="https://arconnect.io" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://arconnect.io/og.png" />
+        <meta property="og:image:alt" content="ArConnect is the first wallet for Arweave, providing secure wallet and asset management" />
+        <meta property="og:locale" content="en_IE" />
+        <meta property="og:site_name" content="ArConnect" />
+      </Head>
+    </>
+  );
 }
