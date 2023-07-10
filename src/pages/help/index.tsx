04309d6fb02e0423731eabd9c5280cc392bc44b7
Marton Lederer <marton@lederer.hu>
2023-07-10 14:24:31 +0100

    feat: search input

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -1,4 +1,5 @@
-import { Space_Grotesk } from "next/font/google";
+import { Manrope, Space_Grotesk } from "next/font/google";
+import { SearchIcon } from "@iconicicons/react";
 import Footer from "~/components/Footer";
 import Spacer from "~/components/Spacer";
 import styled from "styled-components";
@@ -8,6 +9,9 @@ import Nav from "~/components/Nav";
 const spacegrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: "600"
+});
+const manrope = Manrope({
+  subsets: ["latin"]
 });
 
 export default function Help() {
@@ -24,6 +28,11 @@ export default function Help() {
           <Title>
             Knowledge Base
           </Title>
+          <Spacer y={1.6} />
+          <SearchBox>
+            <SearchInput type="text" placeholder="Search for questions, articles or keywords..." />
+            <SearchIcon />
+          </SearchBox>
         </TitleSection>
       </Main>
       <Footer />
@@ -57,8 +66,48 @@ const Question = styled.p`
 
 const Title = styled.h1`
   font-size: 2.85rem;
-  font-weight: 600;
+  font-weight: 700;
   color: rgb(${props => props.theme.primaryText});
   text-align: center;
   margin: 0;
+`;
+
+const SearchBox = styled.div`
+  position: relative;
+  width: max-content;
+  background-color: rgb(${props => props.theme.background});
+  overflow: hidden;
+  border-radius: 14px;
+  margin: 0 auto;
+  z-index: 1;
+
+  svg {
+    position: absolute;
+    left: 1.2rem;
+    top: 50%;
+    width: 1.5rem;
+    height: 1.5rem;
+    z-index: 2;
+    color: rgba(${(props) => props.theme.secondaryText}, 0.45);
+    transform: translateY(-50%);
+  }
+`;
+
+const SearchInput = styled.input`
+  ${manrope.style}
+  position: relative;
+  font-size: 1rem;
+  font-weight: 700;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  padding: 1.2rem 1rem;
+  padding-left: calc(1rem * 2 + 1.5rem);
+  z-index: 3;
+  width: 500px;
+  color: rgb(${(props) => props.theme.secondaryText});
+
+  &::placeholder {
+    color: rgba(${(props) => props.theme.secondaryText}, 0.45);
+  }
 `;
