6c0205f610d291aeb9591df6065afd6e79438aa3
Marton Lederer <marton@lederer.hu>
2023-07-10 14:56:24 +0100

    feat: articles

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -1,10 +1,13 @@
+import { ArrowUpRightIcon, SearchIcon, TrendingUpIcon } from "@iconicicons/react";
 import { Manrope, Space_Grotesk } from "next/font/google";
-import { SearchIcon } from "@iconicicons/react";
+import { Subtitle } from "~/components/content/Text";
+import Section from "~/components/content/Section";
 import Footer from "~/components/Footer";
 import Spacer from "~/components/Spacer";
 import styled from "styled-components";
 import Head from "~/components/Head";
 import Nav from "~/components/Nav";
+import Link from "next/link";
 
 const spacegrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -34,6 +37,88 @@ export default function Help() {
             <SearchIcon />
           </SearchBox>
         </TitleSection>
+        <Spacer y={3} />
+        <Section>
+          <SectionTitle>
+            <TrendingUpIcon />
+            Popular articles
+          </SectionTitle>
+          <Spacer y={2.4} />
+          <Articles>
+            <Article>
+              <ArticleTitle>
+                This is the article title
+              </ArticleTitle>
+              <ArticlePreview>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam a ut aliquam maxime assumenda dolor veritatis non blanditiis eos, quisquam facere rem accusantium, error praesentium suscipit eligendi unde ducimus deserunt.
+              </ArticlePreview>
+              <ArticleLink href="/help/article/1">
+                Read more
+                <ArrowUpRightIcon />
+              </ArticleLink>
+            </Article>
+            <Article>
+              <ArticleTitle>
+                This is the article title
+              </ArticleTitle>
+              <ArticlePreview>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam a ut aliquam maxime assumenda dolor veritatis non blanditiis eos, quisquam facere rem accusantium, error praesentium suscipit eligendi unde ducimus deserunt.
+              </ArticlePreview>
+              <ArticleLink href="/help/article/1">
+                Read more
+                <ArrowUpRightIcon />
+              </ArticleLink>
+            </Article>
+            <Article>
+              <ArticleTitle>
+                This is the article title
+              </ArticleTitle>
+              <ArticlePreview>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam a ut aliquam maxime assumenda dolor veritatis non blanditiis eos, quisquam facere rem accusantium, error praesentium suscipit eligendi unde ducimus deserunt.
+              </ArticlePreview>
+              <ArticleLink href="/help/article/1">
+                Read more
+                <ArrowUpRightIcon />
+              </ArticleLink>
+            </Article>
+            <Article>
+              <ArticleTitle>
+                This is the article title
+              </ArticleTitle>
+              <ArticlePreview>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam a ut aliquam maxime assumenda dolor veritatis non blanditiis eos, quisquam facere rem accusantium, error praesentium suscipit eligendi unde ducimus deserunt.
+              </ArticlePreview>
+              <ArticleLink href="/help/article/1">
+                Read more
+                <ArrowUpRightIcon />
+              </ArticleLink>
+            </Article>
+            <Article>
+              <ArticleTitle>
+                This is the article title
+              </ArticleTitle>
+              <ArticlePreview>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam a ut aliquam maxime assumenda dolor veritatis non blanditiis eos, quisquam facere rem accusantium, error praesentium suscipit eligendi unde ducimus deserunt.
+              </ArticlePreview>
+              <ArticleLink href="/help/article/1">
+                Read more
+                <ArrowUpRightIcon />
+              </ArticleLink>
+            </Article>
+            <Article>
+              <ArticleTitle>
+                This is the article title
+              </ArticleTitle>
+              <ArticlePreview>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam a ut aliquam maxime assumenda dolor veritatis non blanditiis eos, quisquam facere rem accusantium, error praesentium suscipit eligendi unde ducimus deserunt.
+              </ArticlePreview>
+              <ArticleLink href="/help/article/1">
+                Read more
+                <ArrowUpRightIcon />
+              </ArticleLink>
+            </Article>
+          </Articles>
+        </Section>
       </Main>
       <Footer />
     </>
@@ -109,5 +194,67 @@ const SearchInput = styled.input`
 
   &::placeholder {
     color: rgba(${(props) => props.theme.secondaryText}, 0.45);
+  }
+`;
+
+const SectionTitle = styled(Subtitle)`
+  display: flex;
+  align-items: center;
+  gap: 1.4rem;
+  font-size: 1.85rem;
+  font-weight: 600;
+  color: rgb(${props => props.theme.primaryText});
+
+  svg {
+    width: 2.63rem;
+    height: 2.63rem;
+  }
+`;
+
+const Articles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 4.5rem 3.5rem;
+`;
+
+const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const ArticleTitle = styled.h3`
+  font-size: 1.55rem;
+  font-weight: 600;
+  color: rgb(${props => props.theme.secondaryText});
+  margin: 0;
+  line-height: 1.1em;
+`;
+
+const ArticlePreview = styled.p`
+  font-size: .95rem;
+  font-weight: 600;
+  color: rgba(${props => props.theme.secondaryText}, .8);
+  margin: 0;
+  text-align: justify;
+`;
+
+const ArticleLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: .34rem;
+  font-size: 1rem;
+  font-weight: 650;
+  color: rgb(${props => props.theme.primaryText});
+  text-decoration: none;
+  transition: all .23s ease-in-out;
+
+  &:hover {
+    opacity: .77;
+  }
+
+  svg {
+    width: 1.3rem;
+    height: 1.3rem;
   }
 `;
