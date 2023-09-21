b476b63b9f7a7261f53179adc9a6883f8639d418
nicholas ma <nicholas.maa@gmail.com>
2023-09-20 21:02:48 -0700

    fix: rendering blog posts from json wip

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -11,13 +11,31 @@ import { Date, Paragraph, Subtitle } from "~/components/content/Text";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 
+type BlogData = {
+  blogTitle: string;
+  subtitle: string;
+  author: string;
+  datePublished: string;
+  image: string;
+  articleID: string;
+  article: Array<{
+    type: string;
+    title: string;
+    content: Array<{
+      type: string;
+      text?: string;
+      items?: Array<string>;
+    }>;
+  }>;
+};
+
 export default function BlogPost() {
   const router = useRouter();
   const { id } = router.query;
 
   const blogId = id ? String(id) : null;
 
-  const [blogData, setBlogData] = useState(null);
+  const [blogData, setBlogData] = useState<BlogData | null>(null);
 
   useEffect(() => {
     if (blogId) {
@@ -31,94 +49,112 @@ export default function BlogPost() {
           }
         })
         .catch((error) => {
-          console.error("err", error);
+          console.error("Error fetching blog data:", error);
         });
     }
   }, [blogId]);
 
   return (
     <>
-      <Head title="Blog - ArConnect Arweave Wallet" />
-      <Nav />
-      <main>
-        <Section>
-          <Date padding="17px 0px" secondary>
-            <CalendarIcon />
-            Jul 7, 2023
-          </Date>
-          <Title style={{ paddingBottom: "27px" }}>
-            ArConnect adds support for Google Accounts through Othent.
-          </Title>
-          <AuthorGroup>
-            <Image
-              src="/logo.png"
-              alt="ArConnect hedgehog logo"
-              draggable={false}
-              width={56}
-              height={56}
-            />
-            <div>
-              <AuthorText>Marton Lederer</AuthorText>
-              <AuthorText secondary fontSize="16px" fontWeight={600}>
-                8 min read
-              </AuthorText>
-            </div>
-          </AuthorGroup>
-          <ImageContainer style={{ padding: "57px 0" }}>
-            <Image
-              src="/OthentExample.png"
-              alt="Othent logo"
-              draggable={false}
-              width={468}
-              height={151}
-            />
-          </ImageContainer>
-        </Section>
-        <BlogSection>
-          <Paragraph>
-            Lorem ipsum dolor sit amet consectetur. Lacinia congue risus turpis
-            ullamcorper cursus mattis. Nunc tellus euismod nisl aliquam
-            maecenas. Ornare feugiat urna integer in mauris lacinia ut eu.
-            Adipiscing scelerisque sapien id nunc. Ultrices nisl etiam
-            adipiscing eu arcu vitae nisi. Risus aliquet nisl turpis suspendisse
-            at ut elementum at. Malesuada nec morbi sed felis. Odio sit sed non
-            sed facilisi morbi venenatis pretium donec. Erat quis ut sit neque
-            volutpat.
-          </Paragraph>
-          <BlogSubtitle>This is a subtitle</BlogSubtitle>
-          <Paragraph>
-            Lorem ipsum dolor sit amet consectetur. Lacinia congue risus turpis
-            ullamcorper cursus mattis. Nunc tellus euismod nisl aliquam
-            maecenas. Ornare feugiat urna integer in mauris lacinia ut eu.
-            Adipiscing scelerisque sapien id nunc. Ultrices nisl etiam
-            adipiscing eu arcu vitae nisi. Risus aliquet nisl turpis suspendisse
-            at ut elementum at. Malesuada nec morbi sed felis. Odio sit sed non
-            sed facilisi morbi venenatis pretium donec. Erat quis ut sit neque
-            volutpat. Lorem ipsum dolor sit amet consectetur. Lacinia congue
-            risus turpis ullamcorper cursus mattis. Nunc tellus euismod nisl
-            aliquam maecenas. Ornare feugiat urna integer in mauris lacinia ut
-            eu. Adipiscing scelerisque sapien id nunc. Ultrices nisl etiam
-            adipiscing eu arcu vitae nisi. Risus aliquet nisl turpis suspendisse
-            at ut elementum at. Malesuada nec morbi sed felis. Odio sit sed non
-            sed facilisi morbi venenatis pretium donec. Erat quis ut sit neque
-            volutpat.
-          </Paragraph>
-          <Paragraph>
-            Lorem ipsum dolor sit amet consectetur. Lacinia congue risus turpis
-            ullamcorper cursus mattis. Nunc tellus euismod nisl aliquam
-            maecenas. Ornare feugiat urna integer in mauris lacinia ut eu.
-            Adipiscing scelerisque sapien id nunc. Ultrices nisl etiam
-            adipiscing eu arcu vitae nisi. Risus aliquet nisl turpis suspendisse
-            at ut elementum at. Malesuada nec morbi sed felis. Odio sit sed non
-            sed facilisi morbi venenatis pretium donec. Erat quis ut sit neque
-            volutpat.
-          </Paragraph>
-        </BlogSection>
-      </main>
+      {blogData && blogId && (
+        <>
+          <Head title="Blog - ArConnect Arweave Wallet" />
+          <Nav />
+          <main>
+            <Section>
+              <Date padding="17px 0px" secondary>
+                <CalendarIcon />
+                Jul 7, 2023
+              </Date>
+              <Title style={{ paddingBottom: "27px" }}>
+                {blogData.blogTitle}
+              </Title>
+              <AuthorGroup>
+                <Image
+                  src="/logo.png"
+                  alt="ArConnect hedgehog logo"
+                  draggable={false}
+                  width={56}
+                  height={56}
+                />
+                <div>
+                  <AuthorText>{blogData.author}</AuthorText>
+                  <AuthorText secondary fontSize="16px" fontWeight={600}>
+                    8 min read
+                  </AuthorText>
+                </div>
+              </AuthorGroup>
+              <ImageContainer style={{ padding: "57px 0" }}>
+                <Image
+                  src={blogData.image}
+                  alt="Othent logo"
+                  draggable={false}
+                  width={468}
+                  height={151}
+                />
+              </ImageContainer>
+            </Section>
+            <BlogSection>
+              {blogData.article.map((section, indx) => (
+                <div key={indx}>
+                  {section.title && (
+                    <BlogSubtitle>{section.title}</BlogSubtitle>
+                  )}
+                  {section.content.map((contentItem, contentIndex) => {
+                    switch (contentItem.type) {
+                      case "paragraph":
+                        return (
+                          <Paragraph key={contentIndex}>
+                            {contentItem.text}
+                          </Paragraph>
+                        );
+                      case "orderedList":
+                        return (
+                          <List unstyled>
+                            {contentItem.items?.map((item, indx) => (
+                              <ListItem key={indx}>{item}</ListItem>
+                            ))}
+                          </List>
+                        );
+                      case "unorderedList":
+                        return (
+                          <List>
+                            {contentItem.items?.map((item, indx) => (
+                              <ListItem key={indx}>{item} yo</ListItem>
+                            ))}
+                          </List>
+                        );
+                      default:
+                        return null;
+                    }
+                  })}
+                </div>
+              ))}
+            </BlogSection>
+          </main>
+        </>
+      )}
       <Footer />
     </>
   );
 }
+
+const List = styled.ul<{ unstyled?: boolean }>`
+  list-style-type: ${(props) => props.unstyled && "none"};
+`;
+
+// TODO:
+const ListItem = styled.li`
+  ${Paragraph};
+  font-size: 1.05rem;
+  font-weight: 550;
+
+  a {
+    color: rgb(${(props) => props.theme.accent});
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
 
 const BlogSubtitle = styled.h2`
   font-weight: 800;
