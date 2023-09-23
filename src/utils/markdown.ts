bcda9e0a1e0453b35e8b050a8b345a578b3e11e9
Marton Lederer <marton@lederer.hu>
2023-09-23 14:36:11 +0200

    chore: downgrade remark

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,9 +1,8 @@
+import remarkHtml from "remark-html";
 import { remark } from "remark";
-import html from "remark-html";
 
 export default async function markdownToHtml(markdown: string) {
-  // @ts-expect-error
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(remarkHtml).process(markdown);
 
   return result.toString();
 }
