eaf946db746bda97ae5137f1d98460054ad9d839
nicholas ma <nicholas.maa@gmail.com>
2024-07-20 06:25:03 -0700

    chore: removed upper limit

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -8,7 +8,7 @@ export function formatStateVal(val: number) {
     return val;
   } else if (val >= 1000 && val < 1_000_000) {
     return (val / 1000).toFixed() + "K";
-  } else if (val >= 1_000_000 && val < 1_000_000_000) {
+  } else if (val >= 1_000_000) {
     return (val / 1_000_000).toFixed() + "M";
   }
 }
